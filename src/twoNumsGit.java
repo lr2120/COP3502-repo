5a7bfff16fa14771648eb49240a21add29f777ec
Liliana Reyes <lr2120@gmail.com>
2022-03-14 18:29:49 -0400

    add code

--- src/twoNumsGit.java
+++ src/twoNumsGit.java
@@ -1,2 +1,16 @@
-public class twoNumsGit {
+import java.util.Scanner;
+
+public class twoNumsGit
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int firstNum = keyboard.nextInt();
+        System.out.print("Enter second number: ");
+        int secondNum = keyboard.nextInt();
+        System.out.print(firstNum + " - " + secondNum + " = ");
+        System.out.println(firstNum - secondNum);
+    }
 }
